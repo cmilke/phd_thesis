2db3feff50cabb2abb4d46aa027553552db8501f
Chris Milke <chrisdmilke@gmail.com>
2021-12-03 16:33:00 -0600

    further work on stats stuff

--- scripts/stats_test/dice_roll.py
+++ scripts/stats_test/dice_roll.py
@@ -3,15 +3,134 @@ import sys
 import random
 import math
 import statistics
+import sympy
 import numpy
 import scipy.stats
 from matplotlib import pyplot as plt
 
 _die_faces = 6
+_biased_num = 1
+
+def calculate_max_bias(vals=None, bgd_est=None, bins=None, prob=None):
+    # Get probability function vector
+    bias = sympy.Symbol('b')
+
+    signal_total_P = _die_faces+bias-1
+    expec_sig_vals = numpy.array([ 1 if i != _biased_num else bias for i in range(_die_faces) ])
+    expec_sig_vals = expec_sig_vals/signal_total_P * (vals.sum()-bgd_est.sum())
+    expec_vals = expec_sig_vals+bgd_est
+
+    theoretical_pdf = expec_vals / expec_vals.sum()
+    probability_vector = [ prob(vals.sum(), v, p) for v,p in zip(vals, theoretical_pdf) ]
+
+    # Get log likelihood
+    variadic_loglikelihood = -sympy.log(numpy.product(probability_vector))
+    #print([ f'{n}: {float(variadic_loglikelihood.subs(bias,n)):.02f}' for n in numpy.arange(1,5,0.5) ])
+
+    # Take derivative of log likelihood
+    variadic_derivative = sympy.diff(variadic_loglikelihood, bias)
+
+    # Find zero of derivative
+    zeros = []
+    for guess in numpy.arange(1,6,0.5):
+        try:
+            zero = sympy.solvers.solvers.nsolve(variadic_derivative, bias, guess)
+            zeros.append(float(zero))
+        except ValueError: pass
+    if len(zeros) == 0: return None
+    zero = statistics.mean(zeros)
+    return zero
+
+
+def evaluate_max_likelihood(vals=None, bgd_est=None, bins=None, name_prefix=''):
+    # Define probability function
+    probability_function = lambda N,n,p: sympy.functions.combinatorial.factorials.binomial(N,n) * p**n * (1-p)**(N-n)
+    maximum_bias = calculate_max_bias(vals=vals, bgd_est=bgd_est, bins=bins, prob=probability_function)
+    print(f'Bias Estimate = {maximum_bias:.02f}')
+
+    #signal_total_P = _die_faces+maximum_bias-1
+    #expec_sig_vals = numpy.array([ 1 if i != _biased_num else maximum_bias for i in range(_die_faces) ])
+    #expec_sig_vals = expec_sig_vals/signal_total_P * vals.sum()
+    #expec_vals = expec_sig_vals+bgd_est
+
+    #total_events = vals.sum()
+    #num_toys = 100
+    #toy_bias_list = []
+    #for i in range(num_toys):
+    #    toy_distro = numpy.random.choice(bins[:-1], size=total_events, p=expec_vals/expec_vals.sum())
+    #    hist, _ = numpy.histogram(toy_distro, bins=bins)
+    #    toy_bias = calculate_max_bias(vals=hist, bgd_est=bgd_est, bins=bins, prob=probability_function)
+    #    if toy_bias is not None and toy_bias > 0:
+    #        toy_bias_list.append(toy_bias)
+    #    if i%10==0: print(f'{i}', end=' ', flush=True)
+    #print()
+    #bias_avg = statistics.mean(toy_bias_list)
+    #bias_std = statistics.stdev(toy_bias_list)
+
+    #fig, ax = plt.subplots()
+    #ax.hist(toy_bias_list)
+    #ax.axvline(x=maximum_bias, color='purple')
+    #ax.axvline(x=bias_avg+0*bias_std, color='blue', ls='--')
+    #ax.axvline(x=bias_avg+1*bias_std, color='cyan', ls='--')
+    #ax.axvline(x=bias_avg+2*bias_std, color='green', ls='--')
+    #ax.axvline(x=bias_avg+3*bias_std, color='yellow', ls='--')
+    #ax.axvline(x=bias_avg+4*bias_std, color='red', ls='--')
+    #ax.axvline(x=bias_avg+5*bias_std, color='maroon', ls='--')
+    #ax.axvline(x=bias_avg-1*bias_std, color='cyan', ls='--')
+    #ax.axvline(x=bias_avg-2*bias_std, color='green', ls='--')
+    #ax.axvline(x=bias_avg-3*bias_std, color='yellow', ls='--')
+    #ax.axvline(x=bias_avg-4*bias_std, color='red', ls='--')
+    #ax.axvline(x=bias_avg-5*bias_std, color='maroon', ls='--')
+    #print(f'Bias Estimate = {maximum_bias:.02f}+-{bias_std:.02f} ({100*bias_std/maximum_bias:.1f}% Error)')
+
+    #plt.savefig(name_prefix+'_biasEstimate.pdf')
+    return maximum_bias
+
+
+def draw_max_likelihood(vals=None, expec_vals=None, predicted_bias=None, yrange=None, bins=None, name_prefix=''):
+    total_events = vals.sum()
+    num_toys = 1000
+    toy_hists = []
+    for i in range(num_toys):
+        toy_distro = numpy.random.choice(bins[:-1], size=total_events, p=expec_vals/expec_vals.sum())
+        hist, _ = numpy.histogram(toy_distro, bins=bins)
+        toy_hists.append(hist)
+    toy_hists = numpy.array(toy_hists)
+
+    toy_averages = numpy.average(toy_hists, axis=0)
+    toy_stdevs = numpy.std(toy_hists, axis=0)
+    #print(' '.join([f'{v:.02f}' for v in toy_averages]))
+    #print(' '.join([f'{v:.02f}' for v in toy_stdevs]))
+    toy_llvs = -numpy.log(scipy.stats.norm.pdf(toy_hists, loc=toy_averages, scale=toy_stdevs)).sum(axis=1)
+    llv_avg = numpy.average(toy_llvs)
+    llv_std = numpy.std(toy_llvs)
+
+    vals_llv = -numpy.log(scipy.stats.norm.pdf(vals, loc=toy_averages, scale=toy_stdevs)).sum()
+    vals_llv_zval = (vals_llv - llv_avg) / llv_std
+    if vals_llv_zval > 2:
+        print(f'Predicted Bias of {predicted_bias} excluded at {vals_llv_zval:.02f} sigma')
+    else:
+        print(f'Predicted Bias of {predicted_bias} compatible at {vals_llv_zval:.02f} sigma')
+
+
+    fig, ax = plt.subplots()
+    ax.hist(toy_llvs)
+    ax.axvline(x=vals_llv, color='purple')
+    ax.axvline(x=llv_avg, color='blue', ls='--')
+    ax.axvline(x=llv_avg+1*llv_std, color='cyan', ls='--')
+    ax.axvline(x=llv_avg+2*llv_std, color='green', ls='--')
+    ax.axvline(x=llv_avg+3*llv_std, color='yellow', ls='--')
+    ax.axvline(x=llv_avg+4*llv_std, color='red', ls='--')
+    ax.axvline(x=llv_avg+5*llv_std, color='maroon', ls='--')
+
+    #ax.grid()
+    #ax.set_ylim(yrange)
+
+    plt.savefig(name_prefix+'_LogLikeLihood.pdf')
+    return expec_vals, toy_stdevs
 
 
 def draw_sig_bgd(vals=None, err=None, expec_vals=None, expec_err=None, yrange=None, bins=None, name_prefix=''):
-    total_events = vals.sum()
 
     # Analytic Confidence Interval Check
     #p = expec_vals[0]/total_events
@@ -47,29 +166,10 @@ def draw_sig_bgd(vals=None, err=None, expec_vals=None, expec_err=None, yrange=No
     #else:
     #    print('Compatible')
 
-
-    num_toys = 1000
-    toy_hists = []
-    for i in range(num_toys):
-        toy_distro = numpy.random.choice(bins[:-1], size=total_events, p=expec_vals/expec_vals.sum())
-        hist, _ = numpy.histogram(toy_distro, bins=bins)
-        toy_hists.append(hist)
-    toy_hists = numpy.array(toy_hists)
-
-    toy_averages = numpy.average(toy_hists, axis=0)
-    toy_stdevs = numpy.std(toy_hists, axis=0)
-    toy_llvs = numpy.log(scipy.stats.norm.pdf(toy_hists, loc=toy_averages, scale=toy_stdevs)).sum(axis=1)
-    vals_llv = numpy.log(scipy.stats.norm.pdf(vals, loc=toy_averages, scale=toy_stdevs)).sum()
-    pvalue = (toy_llvs < vals_llv).sum() / num_toys
-    print(pvalue, pvalue < .05)
-    exit()
-
-        
-
     fig, axes = plt.subplots(2, gridspec_kw={'height_ratios':(4,1)}, sharex=True)
 
     axes[0].errorbar(bins[:-1], vals,  marker='.', ls='-', color='purple', capsize=2, drawstyle='steps-mid', label='vals')
-    axes[0].errorbar(bins[:-1], expec_vals, yerr=toy_stdev, marker=',', ls='-', color='red', capsize=2, elinewidth=.5, drawstyle='steps-mid', label='expectation')
+    axes[0].errorbar(bins[:-1], expec_vals, yerr=expec_err, marker=',', ls='-', color='red', capsize=2, elinewidth=.5, drawstyle='steps-mid', label='expectation')
     #axes[0].errorbar(bins[:-1], expec_sig_vals, marker=',', ls='--', color='blue')
     #axes[0].errorbar(bins[:-1], expec_bgd_vals, yerr=expec_bgd_err, marker=',', ls='--', color='green')
     #axes[0].hlines(conf95_lo, bins[0], bins[-2], linestyle='-', color='black')
@@ -91,95 +191,49 @@ def draw_sig_bgd(vals=None, err=None, expec_vals=None, expec_err=None, yrange=No
     #rat.set_xticks(rat.get_xticks()+0.5, minor=True)
     #rat.set_xticklabels('') # Clear major tick labels
     #rat.set_xticklabels(rat.get_xticks(), minor=True, ha='center', fontsize=12)
-    #axes[0].set_title(f'Biased Face = {biased_num}')
+    #axes[0].set_title(f'Biased Face = {_biased_num}')
 
     plt.savefig(name_prefix+'_hist_rolls.pdf')
 
 
-def draw_max_likelihood(vals=None, err=None, bgd=None, yrange=None, bins=None, name_prefix=''):
-    mu = numpy.linspace(-100,100,100)
-    #logSeries = - numpy.log(numpy.outer(mu,vals)+bgd)
-    logSeries = (numpy.outer(mu,vals)+bgd)
-    logSum = logSeries.sum(axis=1)
-
-    fig, ax = plt.subplots()
-    ax.errorbar(mu, logSum, marker='.', ls='--', color='purple')
-
-    #ax.grid()
-    #ax.set_ylim(yrange)
-
-    plt.savefig(name_prefix+'_LogLikeLihood.pdf')
 
 
-def roll(biased_num, bias_amount):
-    if biased_num is None:
-        roll = random.randint(1,_die_faces)
+def roll(_biased_num, bias_amount):
+    if _biased_num is None:
+        roll = random.randint(0,_die_faces-1)
     else:
-        val = random.uniform(1,_die_faces+bias_amount)
-        roll = int(val) if val <= _die_faces+1 else biased_num
+        val = random.uniform(0,_die_faces+bias_amount-1)
+        roll = int(val) if val < _die_faces else _biased_num
         
     return roll
 
 
 def main():
-    num_bgd_dice = 0
-    biased_num = 1
+    num_bgd_dice = 3
     bias_amount = 2
-    predicted_bias = 1
-    num_rolls = 60
-    var_edges = range(1,_die_faces+2)
+    predicted_bias = 2
+    num_rolls = 600
+    var_edges = range(0,_die_faces+1)
     name_prefix = 'dice'
 
-
-    signal_rolls = [ roll(biased_num, bias_amount) for i in range(num_rolls) ]
+    signal_rolls = [ roll(_biased_num, bias_amount) for i in range(num_rolls) ]
     bgd_rolls = [ roll(None,None) for i in range(num_rolls*num_bgd_dice) ]
     all_rolls = numpy.array(signal_rolls + bgd_rolls)
-
 
     vals, _ = numpy.histogram(all_rolls, bins=var_edges)
     err = numpy.sqrt(vals)
 
-
     signal_total_P = _die_faces+predicted_bias-1
-    expec_sig_vals = numpy.array([ 1 if i+1 != biased_num else predicted_bias for i in range(_die_faces) ])
+    expec_sig_vals = numpy.array([ 1 if i != _biased_num else predicted_bias for i in range(_die_faces) ])
     expec_sig_vals = expec_sig_vals/signal_total_P * num_rolls
+    bgd_estimate, _ = numpy.histogram(var_edges[:-1], weights=[num_bgd_dice*num_rolls/_die_faces]*_die_faces, bins=var_edges)
+    expec_vals = expec_sig_vals+bgd_estimate
 
-    expec_bgd_vals = numpy.full(_die_faces, 1/_die_faces) * len(bgd_rolls)
-    expec_bgd_err  = numpy.full(_die_faces, math.sqrt(num_bgd_dice*num_rolls) )
-
-    expec_vals = expec_sig_vals#+expec_bgd_vals
-    expec_err = expec_bgd_err
-
+    max_likelihood_bias = evaluate_max_likelihood(vals=vals, bgd_est=bgd_estimate, bins=var_edges, name_prefix=name_prefix)
+    expec_err = draw_max_likelihood(vals=vals, expec_vals=expec_vals, predicted_bias=predicted_bias, bins=var_edges, name_prefix=name_prefix)
     draw_sig_bgd(vals=vals, err=err, expec_vals=expec_vals, expec_err=expec_err, bins=var_edges, name_prefix=name_prefix)
-    #draw_max_likelihood(vals=vals, bgd=expec_bgd_vals, bins=var_edges, name_prefix=name_prefix)
 
-
-def alt():
-    p = 0.5
-    q = 1 - p
-    N = 6
-    L = 0
-    for l in range(int(N/2),-1,-1):
-        m = N - l
-        Pt = 0
-        n1 = 0
-        for n in range(0,l+1):
-            P = math.comb(N,n) * p**n * q**(N-n)
-            Pt += P
-            n1 += 1
-        n2 = 0
-        for n in range(m,N+1):
-            P = math.comb(N,n) * p**n * q**(N-n)
-            Pt += P
-            n2 += 1
-        print(f' 0-{l:2}:{n1:2}, {m:2}-{N:2}:{n2:2} / {N}: {Pt:.04f}')
             
 
 
-
-
-
-    
-
 main()
-#alt()
--- scripts/stats_test/vbf_4b_stats.py
+++ scripts/stats_test/vbf_4b_stats.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 import sys
+import math
 import random
 import statistics
 import pickle
+import scipy
+import scipy.stats
+import sympy
 import numpy
 import uproot
 from matplotlib import pyplot as plt
@@ -78,16 +82,16 @@ def load_bgd(var_edges):
 
 
 def load_signal(var_edges):
-    #signal_files = [
-    #    '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502970_MC16a-2015-2016_NanoNTuple.root',
-    #    '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502970_MC16d-2017_NanoNTuple.root',
-    #    '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502970_MC16e-2018_NanoNTuple.root'
-    #]
     signal_files = [
-        '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502975_MC16a-2015-2016_NanoNTuple.root',
-        '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502975_MC16d-2017_NanoNTuple.root',
-        '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502975_MC16e-2018_NanoNTuple.root'
+        '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502970_MC16a-2015-2016_NanoNTuple.root',
+        '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502970_MC16d-2017_NanoNTuple.root',
+        '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502970_MC16e-2018_NanoNTuple.root'
     ]
+    #signal_files = [
+    #    '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502975_MC16a-2015-2016_NanoNTuple.root',
+    #    '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502975_MC16d-2017_NanoNTuple.root',
+    #    '/home/cmilke/Documents/dihiggs/nano_ntuples/2021_aug_test/VBF_nonres2021_502975_MC16e-2018_NanoNTuple.root'
+    #]
 
     key='m_hh'
     events = []
@@ -122,22 +126,134 @@ def load_signal(var_edges):
     return (weights, errors)
 
 
+def load_fake_bgd(data_vals, data_errs, sig_vals, sig_errs):
+    print()
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print('!!!    LOADING FAKE BACKGROUND    !!!')
+    print('!!! ALL RESULTS ARE COMPLETE LIES !!!')
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print()
+
+    #ideal_bgd = data_vals-sig_vals # to make Signal compatible with data
+    #ideal_bgd = data_vals # to make signal incompatible with data
+    ideal_bgd = data_vals*1.1 # to make signal very incompatible with data
+    fake_errs = data_errs
+    jitter_amount = 0.005 # Decrease this to increase result significance
+    jittered_fake_bgd = scipy.stats.norm.rvs(loc=ideal_bgd, scale=ideal_bgd*jitter_amount)
+    return (jittered_fake_bgd, fake_errs*jitter_amount)
+
+
+def make_data_display_plots(data=None, var_edges=None):
+    fig, (ax, rat) = plt.subplots(2, gridspec_kw={'height_ratios':(1,1)}, sharex=True)
+    ax.errorbar(var_edges[:-1]+0.5, data['data'][0], yerr=data['data'][1], marker='.', ls='--', color='purple', label='Data')
+    ax.errorbar(var_edges[:-1]+0.5, data['bgd'][0], yerr=data['bgd'][1], marker='.', ls='--', color='blue', label='Bgd')
+    ax.errorbar(var_edges[:-1]+0.5, data['sig'][0], yerr=data['sig'][1], marker='.', ls='--', color='green', label='Signal')
+
+    rat.errorbar(var_edges[:-1]+0.5, data['data'][0]-data['bgd'][0],
+        yerr = numpy.sqrt(data['data'][1]**2 + data['bgd'][1]**2),
+        marker='.', ls='--', color='red')
+    rat.errorbar(var_edges[:-1]+0.5, data['sig'][0], yerr=data['sig'][1], marker='.', ls='--', color='green')
+    rat.hlines(0, var_edges[0], var_edges[-1], linestyle='-', color='black')
+
+    ax.legend()
+
+
+    plt.savefig('data_dump.pdf')
+    plt.close()
+
+
+def derive_maximum_likelihood_mu(data=None, couplings=None):
+    data_vals, data_errs = data['data']
+    sig_vals, sig_errs = data['sig']
+    bgd_vals, bgd_errs = data['bgd']
+
+    mu = sympy.Symbol('mu')
+    #sig_vector = [ sympy.Symbol(f's{i}') for i in range(len(sig_vals)) ]
+    #bgd_vector = [ sympy.Symbol(f'b{i}') for i in range(len(bgd_vals)) ]
+    #events_observed = sympy.Symbol('n')
+    theoretical_events = lambda s,b: mu*s+b
+    llh = lambda n,T: (n-1)*sympy.log(T) - T - n*sympy.log(n) + n
+    ellh = lambda n,events: llh(n,sum(events)) + sum([sympy.log(e) for e in events])
+
+    theoretical_events_per_bin = [ theoretical_events(sig, bgd) for sig,bgd in zip(sig_vals, bgd_vals) ]
+    extended_log_likelihood_function = ellh(sum(data_vals), theoretical_events_per_bin)
+    #theoretical_events_per_bin = [ theoretical_events(s, b) for s,b in zip(sig_vector, bgd_vector) ]
+    #extended_log_likelihood_function = ellh(events_observed, theoretical_events_per_bin)
+    print(extended_log_likelihood_function)
+    print()
+
+    ell_derivative = sympy.diff(extended_log_likelihood_function, mu)
+    print(ell_derivative)
+    print()
+
+    zero = sympy.solvers.solvers.nsolve(ell_derivative, mu, 200)
+    print(zero)
+
+
+def make_lazy_mu_probability_distro(data=None, couplings=None):
+    data_vals, data_errs = data['data']
+    sig_vals, sig_errs = data['sig']
+    bgd_vals, bgd_errs = data['bgd']
+
+    mu = sympy.Symbol('mu')
+    theoretical_events = lambda s,b: mu*s+b
+    #poisson = lambda n,k: k**n * sympy.exp(-k) / ( (n/math.e)**n * sympy.sqrt(2*math.pi*n) )
+    log_poisson = lambda n,k: n - k + n*sympy.log(k/n) if n > 0 else -k
+
+    theory_vals = [ theoretical_events(sig, bgd) for sig,bgd in zip(sig_vals, bgd_vals) ]
+    loglikelihood = sum( [ log_poisson(data,theory) for data, theory in zip(data_vals, theory_vals) ] )
+    log_prior = 0 # log(1); Uniform Prior, "Principle of Ignorance"
+    log_probability_of_muXprobability_of_data = loglikelihood+log_prior
+
+    mu_log_probability_function = sympy.lambdify(mu, log_probability_of_muXprobability_of_data, 'numpy')
+
+    def get_cumlative_PDF(max_mu=1000, zoom_passes=2, precision_threshold=1e-5):
+        for i in range(zoom_passes+1):
+            mu_values = numpy.linspace(0,max_mu,100)
+            mu_log_probabilities = mu_log_probability_function(mu_values)
+            mu_probabilities = numpy.exp(mu_log_probabilities)
+            mu_probabilities /= mu_probabilities.sum()
+            cumalitive_mu_PDF = mu_probabilities[::-1].cumsum()[::-1]
+            max_mu = mu_values[numpy.argmax(cumalitive_mu_PDF<precision_threshold)]
+        return mu_values, mu_probabilities, cumalitive_mu_PDF
+    mu_values, mu_probabilities, cumalitive_mu_PDF = get_cumlative_PDF()
+    exclusion_limit_index = numpy.argmax(cumalitive_mu_PDF < 0.05)
+    mu_limit = mu_values[exclusion_limit_index]
+
+
+    fig, ax = plt.subplots()
+    ax.errorbar(mu_values, mu_probabilities, color='black', ls='--')
+    ax.axvline(x=mu_limit)
+    plt.savefig('mu_pdf.pdf')
+    plt.close()
+
+    fig, ax = plt.subplots()
+    ax.errorbar(mu_values, cumalitive_mu_PDF, color='black', ls='--')
+    ax.hlines(0.05, mu_values[0], mu_values[-1], color='red', ls='dotted')
+    ax.axvline(x=mu_limit)
+    plt.savefig('mu_Cpdf.pdf')
+    plt.close()
+    print(mu_limit)
+
+
+
 def main():
     cache_file = '.cached_analysis_info.p'
-    var_edges = numpy.linspace(200, 1400, 6)
+    var_edges = numpy.linspace(200, 1400, 30)
 
     ###################
     # LOAD EVERYTHING #
     ###################
     if len(sys.argv) > 1:
+        # Load Signal - Juse use SM and then maybe cvv = 3
+        sig_vals, sig_errs = load_signal(var_edges)
+
         # Load Data - Just load 3b1f Signal Region
         data_vals, data_errs = load_data(var_edges)
 
-        # Load Background - Just use 2b Signal Region
+        # Load Background - Just reweight 2b Signal Region
         bgd_vals, bgd_errs = load_bgd(var_edges)
-
-        # Load Signal - Juse use SM and then maybe cvv = 3
-        sig_vals, sig_errs = load_signal(var_edges)
+        #bgd_vals, bgd_errs = load_fake_bgd(data_vals, data_errs, sig_vals, sig_errs)
 
         data = {
             'data': (data_vals, data_errs),
@@ -155,19 +271,11 @@ def main():
     #    print(' '.join([f'{e:7.02f}' for e in err]))
     #    print()
 
-    fig, (ax, rat) = plt.subplots(2, gridspec_kw={'height_ratios':(1,1)}, sharex=True)
-    ax.errorbar(var_edges[:-1]+0.5, data['data'][0], yerr=data['data'][1], marker='.', ls='--', color='purple')
-    ax.errorbar(var_edges[:-1]+0.5, data['bgd'][0], yerr=data['bgd'][1], marker='.', ls='--', color='blue')
-    ax.errorbar(var_edges[:-1]+0.5, data['sig'][0], yerr=data['sig'][1], marker='.', ls='--', color='green')
-
-    rat.errorbar(var_edges[:-1]+0.5, data['data'][0]-data['bgd'][0],
-        yerr = numpy.sqrt(data['data'][1]**2 + data['bgd'][1]**2),
-        marker='.', ls='--', color='red')
-    rat.errorbar(var_edges[:-1]+0.5, data['sig'][0], yerr=data['sig'][1], marker='.', ls='--', color='green')
-    rat.hlines(0, var_edges[0], var_edges[-1], linestyle='-', color='black')
+    make_data_display_plots(data=data,var_edges=var_edges)
+    make_lazy_mu_probability_distro(data=data, couplings=(1,1,1))
+    #derive_maximum_likelihood_mu(data=data, couplings=(1,1,1))
 
 
-    plt.savefig('data_dump.pdf')
 
 
     
