f2a8e108d2b393eebb55747b589ad2fcb744e721
Chris Milke <chrisdmilke@gmail.com>
2022-04-21 16:35:01 -0500

    finished up Steve's edits

--- scripts/stats_test/render.sh
+++ scripts/stats_test/render.sh
@@ -5,7 +5,7 @@ inputpoints=$1
 label="set xlabel 'k2V';"
 label+="set ylabel 'kl';"
 label+="set zlabel 'kV';"
-gpcommand="$label;splot '$inputpoints' with points pointtype 7 pointsize 0.3;"
+gpcommand="$label;splot '$inputpoints' with points pointtype 7 pointsize 0.3 notitle;"
 gpcommand+="pause -1;"
 
 gnuplot -e "$gpcommand"
